0f4bbe9dffacf87f1d98891108c1aeaab3cfaf79
qplatt <quincyplatt@gmail.com>
2021-11-15 17:53:02 +0000

    Adding heroku

--- aesthetic_accessories/settings.py
+++ aesthetic_accessories/settings.py
@@ -123,7 +123,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -162,6 +161,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
